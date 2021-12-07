fdfca1d859dbc7ea61d60a667e14df4a64c8dc9c
priscila monteiro <pririmonteiro@me.com>
2021-12-07 13:17:21 -0800

    api wired and designed table schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,21 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('cars', table => {
+    table.increments('id')
+    table.text('vin', 128)
+      .unique()
+      .notNullable()
+    table.text('make', 128)
+      .notNullable()
+    table.text('model', 128)
+      .notNullable()
+    table.integer('mileage')
+      .notNullable()
+    table.text('title', 128)
+    table.text('transmission')
+
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
